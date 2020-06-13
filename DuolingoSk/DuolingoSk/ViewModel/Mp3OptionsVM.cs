188aed54e695329a5b224d369e8e2ffbc126d985
krutikshahdev <krutik.shah1310@gmail.com>
2020-06-13 18:27:18 +0530

    Commit

---
+++ DuolingoSk/DuolingoSk/ViewModel/Mp3OptionsVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuolingoSk.ViewModel
+{
+    public class Mp3OptionsVM
+    {
+        public long Mp3OptionId { get; set; }
+        public long QuestionId { get; set; }
+        public string Mp3FileName { get; set; }
+        public string Mp3Text { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ DuolingoSk/DuolingoSk/ViewModel/QuestionVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuolingoSk.ViewModel
+{
+    public class QuestionVM
+    {
+        public long QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public int QuestionTime { get; set; }
+        public int QuestionTypeId { get; set; }
+        public string QuestionOptionText { get; set; }
+        public string Words { get; set; }
+        public string Mp3FileName { get; set; }
+        public string Mp3FileText { get; set; }
+        public int MaxReplay { get; set; }
+        public string ImageName { get; set; }
+        public int NoOfWords { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+        public string QuestionTypeText { get; set; }
+        public List<Mp3OptionsVM> Mp3Options { get; set; }
+    }
+}
