b13cae54c03b3c5ba3dd6178639267912541a152
Krutik Shah <krutik9033331394@outlook.com>
2020-07-05 16:31:41 +0530

    Commit Partially

--- DuolingoSk/DuolingoSk/Areas/Client/Controllers/StartExamController.cs
+++ DuolingoSk/DuolingoSk/Areas/Client/Controllers/StartExamController.cs
@@ -63,7 +63,7 @@ namespace DuolingoSk.Areas.Client.Controllers
             Session["lstQuestionsExam"] = null;
             if (Session["lstQuestionsExam"] == null)
             {
-                List<long> TypIds = _db.tbl_QuestionType.ToList().Select(x => x.QuestionTypeId).ToList();
+                List<long> TypIds = _db.tbl_QuestionType.ToList().Select(x => x.QuestionTypeId).ToList();               
                 List<QuestionVM> lstQuestions = (from p in _db.tbl_QuestionsMaster
                                                  where p.IsDeleted == false && p.IsActive == true && p.QuestionLevel == LevelId
                                                  select new QuestionVM
@@ -78,7 +78,10 @@ namespace DuolingoSk.Areas.Client.Controllers
                                                      Mp3FileName = p.Mp3FileName,
                                                      MaxReplay = p.MaxReplay.HasValue ? p.MaxReplay.Value : 0,
                                                      ImageName = p.ImageName,
-                                                     NoOfWords = p.NoOfWords.HasValue ? p.NoOfWords.Value : 0
+                                                     NoOfWords = p.NoOfWords.HasValue ? p.NoOfWords.Value : 0,
+                                                     PreparationTime = p.PreparationTime.HasValue ? p.PreparationTime.Value : 0,
+                                                     MinimumTime = p.MinimumTime.HasValue ? p.MinimumTime.Value : 0,
+                                                     Images = p.Images
                                                  }).OrderByDescending(x => x.QuestionId).ToList();
 
                   lstQuestions.Where(x => x.QuestionTypeId == 5).ToList().ForEach(x => x.Mp3Options = GetMp3Options(x.QuestionId));
@@ -140,9 +143,25 @@ namespace DuolingoSk.Areas.Client.Controllers
             {
                 return PartialView("~/Areas/Client/Views/StartExam/_QuestionType5.cshtml", objQ);
             }
-            else
+            else if (objQ.QuestionTypeId == 6)
             {
                 return PartialView("~/Areas/Client/Views/StartExam/_QuestionType6.cshtml", objQ);
+            }
+            else if (objQ.QuestionTypeId == 7)
+            {
+                return PartialView("~/Areas/Client/Views/StartExam/_QuestionType7.cshtml", objQ);
+            }
+            else if (objQ.QuestionTypeId == 8)
+            {
+                return PartialView("~/Areas/Client/Views/StartExam/_QuestionType8.cshtml", objQ);
+            }
+            else if (objQ.QuestionTypeId == 9)
+            {
+                return PartialView("~/Areas/Client/Views/StartExam/_QuestionType9.cshtml", objQ);
+            }
+            else
+            {
+                return PartialView("~/Areas/Client/Views/StartExam/_QuestionType10.cshtml", objQ);
             }
         }
         [HttpPost]
@@ -204,7 +223,7 @@ namespace DuolingoSk.Areas.Client.Controllers
 
             }
 
-            return Json(path + Request.Form[0]);
+            return Json(Convert.ToString(Request.Form[0]));
         }
     }
 
