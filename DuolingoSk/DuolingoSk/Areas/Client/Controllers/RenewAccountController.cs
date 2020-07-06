a48afc4f474c4d0899e97e86366ffe7bad6758fe
Nilesh Prajapati <prajapati.nileshbhai@gmail.com>
2020-07-06 21:57:49 +0530

    fee list displayed in renew page

--- DuolingoSk/DuolingoSk/Areas/Client/Controllers/RenewAccountController.cs
+++ DuolingoSk/DuolingoSk/Areas/Client/Controllers/RenewAccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DuolingoSk.Model;
+using DuolingoSk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,39 @@ namespace DuolingoSk.Areas.Client.Controllers
 {
     public class RenewAccountController : Controller
     {
-        // GET: Client/RenewAccount
+        private readonly DuolingoSk_Entities _db;
+
+        public RenewAccountController()
+        {
+            _db = new DuolingoSk_Entities();
+        }
         public ActionResult Index()
         {
-            return View();
+            long LoggedInStudentId = Convert.ToInt64(clsClientSession.UserID);
+
+            List<StudentFeeVM> lstFee = new List<StudentFeeVM>();
+
+            try
+            {
+                 
+                lstFee = (from a in _db.tbl_StudentFee 
+                          where !a.IsDeleted && a.StudentId == LoggedInStudentId
+                          select new StudentFeeVM
+                          {
+                              StudentFeeId = a.StudentFeeId,
+                              StudentId = a.StudentId,
+                              FeeStatus = a.FeeStatus,
+                              FeeAmount = a.FeeAmount,
+                              TotalExamAttempt = a.TotalExamAttempt,
+                              RequestedDate = a.RequestedDate, 
+                          }).OrderBy(x => x.RequestedDate).ToList();
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return View(lstFee);
         }
     }
 }
